1afd4efec9b56f63d62c2f49af501445e14638cb
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-06-24 16:08:53 +0300

    Working on JavaScript functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Follow along with the examples here
+function sayHello() {
+  console.log("Hello!");
+}
+function sayHelloGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`;
+}
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+sayHello();
+sayHelloGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+console.log(say("Hello", "Abel.K"));
+console.log(add(4, 43));
